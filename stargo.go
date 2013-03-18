2c3fa832bd9b26cada2e9ac7e35b1279b859e11a
Nicholas <dawtsf@gmail.com>
2013-03-17 19:44:23 -0700

    new file:   pkg/archive/tar/common.go 	new file:   pkg/archive/tar/example_test.go 	new file:   pkg/archive/tar/reader.go 	new file:   pkg/archive/tar/reader_test.go 	new file:   pkg/archive/tar/stat_atim.go 	new file:   pkg/archive/tar/stat_atimespec.go 	new file:   pkg/archive/tar/stat_unix.go 	new file:   pkg/archive/tar/tar_test.go 	new file:   pkg/archive/tar/testdata/gnu.tar 	new file:   pkg/archive/tar/testdata/pax.tar 	new file:   pkg/archive/tar/testdata/small.txt 	new file:   pkg/archive/tar/testdata/small2.txt 	new file:   pkg/archive/tar/testdata/star.tar 	new file:   pkg/archive/tar/testdata/ustar.tar 	new file:   pkg/archive/tar/testdata/v7.tar 	new file:   pkg/archive/tar/testdata/writer-big.tar 	new file:   pkg/archive/tar/testdata/writer.tar 	new file:   pkg/archive/tar/writer.go 	new file:   pkg/archive/tar/writer_test.go 	new file:   pkg/archive/zip/example_test.go 	new file:   pkg/archive/zip/reader.go 	new file:   pkg/archive/zip/reader_test.go 	new file:   pkg/archive/zip/struct.go 	new file:   pkg/archive/zip/testdata/crc32-not-streamed.zip 	new file:   pkg/archive/zip/testdata/dd.zip 	new file:   pkg/archive/zip/testdata/go-no-datadesc-sig.zip 	new file:   pkg/archive/zip/testdata/go-with-datadesc-sig.zip 	new file:   pkg/archive/zip/testdata/gophercolor16x16.png 	new file:   pkg/archive/zip/testdata/readme.notzip 	new file:   pkg/archive/zip/testdata/readme.zip 	new file:   pkg/archive/zip/testdata/symlink.zip 	new file:   pkg/archive/zip/testdata/test.zip 	new file:   pkg/archive/zip/testdata/unix.zip 	new file:   pkg/archive/zip/testdata/winxp.zip 	new file:   pkg/archive/zip/testdata/zip64.zip 	new file:   pkg/archive/zip/writer.go 	new file:   pkg/archive/zip/writer_test.go 	new file:   pkg/archive/zip/zip_test.go 	new file:   stargo.go

---
+++ stargo.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"io"
+	"os"
+	"fmt"
+	"log"
+	"archive/tar"
+	"compress/gzip"
+	"path/filepath"
+)
+
+type Action func(*tar.Reader) error
+
+func main() {
+	var action byte
+	args := os.Args
+	zflag := false
+
+	if len(args) < 2 || len(args[1]) > 2 || len(args[1]) < 1 { usage() }
+	if args[1][0] == 'z' {
+		if len(args[1]) != 2 {
+			usage()
+		} else {
+			zflag = true
+			action = args[1][1]
+		}
+	} else {
+		action = args[1][0]
+	}
+
+	switch action {
+	case 'c':
+		c(args[2:], zflag)
+	case 'x':
+		targo(x, zflag)
+	case 't':
+		targo(t, zflag)
+	default:
+		usage()
+	}
+}
+
+func usage() {
+	log.Fatal("stargo [z][cxt] [files]\n")
+}
+
+func c(locs []string, zflag bool) {
+
+	c_file := func(loc string, info os.FileInfo, _ error) error {
+		return nil
+	}
+
+	//error checking! also, what about symlinks?  Lstat or Stat?
+	for _, loc := range locs {
+		if fi, err := os.Stat(loc); err == nil {
+			if fi.IsDir() {
+				filepath.Walk(loc, c_file)
+			} else {
+				c_file(loc, fi, nil)
+			}
+		} else if err == filepath.SkipDir {
+			log.Println(err)
+		} else { panic(err) }
+	}
+}
+
+func targo(action Action, zflag bool) {
+
+	var f io.Reader
+
+	if zflag {
+		gr, err := gzip.NewReader(os.Stdin)
+		defer gr.Close()
+		if err != nil { log.Fatal(err) }
+		f = gr
+	} else {
+		f = os.Stdin
+	}
+	tr := tar.NewReader(f)
+	if err := action(tr); err != nil { log.Fatal(err) }
+}
+
+func t(tr *tar.Reader) error {
+
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%s\n", hdr.Name)
+	}
+	return nil //shouldn't get here
+}
+
+func x(tr *tar.Reader) error {
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF { return nil }
+		if err != nil { return err }
+
+		switch hdr.Typeflag {
+		case tar.TypeReg, tar.TypeRegA:
+			f, err := os.OpenFile(hdr.Name, os.O_CREATE, hdr.Mode)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			buf := make([]byte, hdr.Size) //bufio?
+			if _, err := tr.Read(buf); err != nil { return err }
+			if _, err := f.Write(buf); err != nil { return err }
+		case tar.TypeLink:
+			//hard link
+		case tar.TypeSymlink:
+		case tar.TypeChar:
+		case tar.TypeBlock:
+		case tar.TypeDir:
+			if err := os.MkdirAll(hdr.Name, hdr.Mode); err != nil { return err }
+		case tar.TypeFifo:
+		case tar.TypeCont:
+			//reserved
+		case tar.TypeXHeader:
+			//extended header
+		case tar.TypeXGlobalHeader:
+			//extended global header
+		default:
+			log.Printf("Unknown type for %s\n", hdr.Name)
+		}
+		if err := os.Chown(hdr.Name, hdr.Uid, hdr.Gid); err != nil { return err }
+		if err := os.Chmod(hdr.Name, hdr.Mode); err != nil { return err }
+	}
+	return nil //shouldn't get here
+}
